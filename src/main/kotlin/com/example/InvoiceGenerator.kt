0a67dade432da8ac96c25748f8bd1dff0003dba6
Abhinav Manchanda <abhinav@abhinav.local>
2021-07-26 15:07:56 +0530

    Extract constants for fare per km, minute, and booking charges

--- src/main/kotlin/com/example/InvoiceGenerator.kt
+++ src/main/kotlin/com/example/InvoiceGenerator.kt
@@ -1,8 +1,14 @@
 package com.example
 
 class InvoiceGenerator {
+    companion object {
+        val FARE_PER_KM = 10
+        val FARE_PER_MINUTE = 1
+        val BOOKING_CHARGES = 5
+    }
+
     fun totalFare(distance: Double, duration: Double): Double {
-        return distance * 10 + duration * 1 + 5
+        return distance * FARE_PER_KM + duration * FARE_PER_MINUTE + BOOKING_CHARGES
     }
 
 }
