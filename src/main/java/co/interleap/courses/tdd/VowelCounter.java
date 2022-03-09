c4d976576f485f65ae120dc2b7539b8cd3403603
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-09 12:28:15 +0530

    Code Improvement

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,15 +1,24 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+	
 	public int count(String s)
 	{
 		int vowel_count=0;
 		for(int i=0;i<s.length();i++)
 		{
-			if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||
-					s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
+			    char c =s.charAt(i);
+			    if(isVowel(c))
 		         vowel_count++;
 		}
 		return vowel_count;
+	}
+	
+	
+	public boolean isVowel(char c)
+	{
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+			return true;
+		return false;
 	}
 }
