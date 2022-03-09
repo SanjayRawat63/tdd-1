5edeb21076544ac7f0c5050d74a1c6cd818739f4
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-09 12:15:44 +0530

    Passes for having Both UpperCase And LowerCase Characters in String and previous

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -6,7 +6,8 @@ public class VowelCounter {
 		int vowel_count=0;
 		for(int i=0;i<s.length();i++)
 		{
-			if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+			if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||
+					s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
 		         vowel_count++;
 		}
 		return vowel_count;
